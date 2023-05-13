259db8eff4304cc102b3796e6e49064e83b2fca7
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-05-13 17:46:20 +0530

    Using benchmark function to measure the performance

--- src/forAndReduce.js
+++
@@ -1,9 +0,0 @@
-const array = new Array(100000).fill('1');
-const timeBeforeFor = new Date().valueOf();
-
-const incrementedArray = array.reduce((newArr,x) => {newArr.push(x + 1); return newArr}, []);
-
-const timeAfterFor = new Date().valueOf();
-const timeTaken = timeAfterFor - timeBeforeFor;
-
-console.log(timeTaken);
--- src/main.js
+++ src/main.js
@@ -2,16 +2,20 @@ const fs = require("fs");
 const { matchStrings } = require("./match-strings.js");
 
 const main = function () {
+  console.log('1');
   const firstFile = process.argv[2];
   const secondFile = process.argv[3];
 
   const firstFileContents = fs.readFileSync(firstFile, "utf-8");
   const secondFileContents = fs.readFileSync(secondFile, "utf-8");
-  const timeBeforeCall = new Date().valueOf();
   const matchedStrings = matchStrings(firstFileContents, secondFileContents);
-  const timeAfterCall = new Date().valueOf();
-  const timeTaken = timeAfterCall - timeBeforeCall;
-  console.log(timeTaken);
 };
 
-main();
+const benchmark = (fnToBenchmark) => {
+  const before = new Date();
+  const result = fnToBenchmark();
+  const after = new Date();
+  return [result, after - before];
+};
+
+benchmark(() => main());
